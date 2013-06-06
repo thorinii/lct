0653a48428e337bd166c438d9cd794731ea10a70
lachlan <lachlan@Cesium>
2013-06-06 16:04:58 +0930

    Added extra tests to MainTesting

--- src/main/java/me/lachlanap/lct/MainTesting.java
+++ src/main/java/me/lachlanap/lct/MainTesting.java
@@ -11,14 +11,20 @@ import me.lachlanap.lct.gui.LCTFrame;
  */
 public class MainTesting {
 
-    public static void main(String[] args) throws ClassNotFoundException,
-            InstantiationException,
-            IllegalAccessException,
-            UnsupportedLookAndFeelException {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException |
+                IllegalAccessException |
+                InstantiationException |
+                UnsupportedLookAndFeelException e) {
+            // We don't particularly care if the LaF cannot be set
+            e.printStackTrace();
+        }
 
         LCTManager lct = new LCTManager();
         lct.register(ClassOfConstantTesting.class);
+        lct.register(SecondSetOfConstants.class);
 
         LCTFrame frame = new LCTFrame(lct);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,5 +37,13 @@ public class MainTesting {
         public static int NUMBER = 10;
         @IntConstant(name = "Integer", min = 6, max = 8)
         public static int INTEGER = 6;
+    }
+
+    public static class SecondSetOfConstants {
+
+        @IntConstant(name = "Gravity", min = 0, max = 100)
+        public static int GRAVITY = 7;
+        @IntConstant(name = "Drag", min = 0, max = 10)
+        public static int DRAG = 2;
     }
 }
